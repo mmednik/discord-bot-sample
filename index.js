de04f27c1e9e7a88115033e030dcf0c8fe03f901
mmednik <mmednik@gmail.com>
2022-01-14 11:41:21 -0300

    read data from google sheet

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const prefix = "%";
+const { google } = require("googleapis");
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: `keys/${config.GOOGLE_SHEET_API}.json`,
+  scopes: "https://www.googleapis.com/auth/spreadsheets",
+});
+
+let readData;
+
+async function getSheetData() {
+  const authClientObject = await auth.getClient();
+
+  const googleSheetsInstance = google.sheets({
+    version: "v4",
+    auth: authClientObject,
+  });
+  const spreadsheetId = config.SPREADSHEET_ID;
+  
+  readData = await googleSheetsInstance.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:A",
+  });
+}
+
+getSheetData();
+
 
 client.on("messageCreate", function (message) {
   if (message.author.bot) return;
@@ -20,6 +47,9 @@ client.on("messageCreate", function (message) {
       const numArgs = args.map((x) => parseFloat(x));
       const sum = numArgs.reduce((counter, x) => (counter += x));
       message.reply(`La suma de los números es ${sum}!`);
+    case "data":
+      message.reply(`Data: ${JSON.stringify(readData.data)}`);
+      break;
   }
 });
 
